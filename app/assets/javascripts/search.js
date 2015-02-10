6e59274cf6f5ab16b861960df8439a00f9f611b2
zRouth <zrouthier@gmail.com>
2015-02-10 09:00:51 -0700

    implemented js search box

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,18 @@
+$(document).ready(
+  function(){
+    $('#searchbox').keyup(
+      function(){
+      var searchText = $(this).val();
+
+       if (searchText.length > 0) {
+         $('tbody tr').hide()
+         .filter(function (index, element) {
+           return!!(element.innerHTML.match(searchText));
+         }).show();
+       }
+
+       else if (searchText.length == 0) {
+         $('tbody tr').show()
+       }
+    });
+});
